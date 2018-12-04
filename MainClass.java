0b3e422f2e11f000c7456a394bead8e46356112c
Egor <45425255+uralspotter@users.noreply.github.com>
2018-12-04 11:00:37 +0500

    Checkbox test

--- MainClass.java
+++ MainClass.java
@@ -3,35 +3,51 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class MainClass {
 
+    static WebDriver driver_chrome;
+
     public static void main(String[] args) {
 
-        final String site = "http://github.com";
+        final String site = "https://passport.yandex.ru/auth";
 
         System.setProperty("WebDriver.gecko.driver","C:\\Windows\\System32\\geckodriver.exe");
         System.setProperty("WebDriver.chrome.driver", "C:\\Windows\\System32\\chromedriver.exe");
         System.setProperty("WebDriver.microsoft.driver", "C:\\Windows\\System32\\MicrosoftWebDriver.exe");
 
 //        WebDriver driver_firefox = new FirefoxDriver();
-        WebDriver driver_chrome = new ChromeDriver();
+        driver_chrome = new ChromeDriver();
         driver_chrome.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver_chrome.manage().window().maximize();
 //        driver_firefox.get(site);
         driver_chrome.get(site);
         System.out.println(driver_chrome.getTitle());
         System.out.println(driver_chrome.getCurrentUrl());
-        driver_chrome.findElement(By.xpath("//input[@id='user[login]']")).sendKeys("userLogin");
-        driver_chrome.findElement(By.xpath("//input[@id='user[email]']")).sendKeys("userEmail");
-        driver_chrome.findElement(By.xpath("//input[@id='user[password]']")).sendKeys("userPassword");
-        WebElement link = driver_chrome.findElement(By.xpath("//button[@type='submit']"));
-        link.click();
+//        WebElement link = driver_chrome.findElement(By.xpath("//div[@class='desk-notif-card__card']/a[@class='desk-notif-card__login-enter-expanded']"));
+//        link.click();
+        driver_chrome.findElement(By.xpath("//input[@name='login']")).sendKeys("userLogin");
+        driver_chrome.findElement(By.xpath("//input[@name='passwd']")).sendKeys("userPassword");
+        selectedCheckbox();
+        unselectedCheckbox();
         System.out.println(driver_chrome.getTitle());
         System.out.println(driver_chrome.getCurrentUrl());
-        driver_chrome.quit();
+//        driver_chrome.quit();
 //        driver_firefox.quit();
+    }
+
+    public static void selectedCheckbox() {
+        String cbXpath = "//input[@name='twoweeks']";
+        if (!driver_chrome.findElement(By.xpath(cbXpath)).isSelected()) {
+            driver_chrome.findElement(By.xpath(cbXpath)).click();
+        }
+    }
+
+    public static void unselectedCheckbox() {
+        String cbXpath = "//input[@name='twoweeks']";
+        if (driver_chrome.findElement(By.xpath(cbXpath)).isSelected()) {
+            driver_chrome.findElement(By.xpath(cbXpath)).click();
+        }
     }
 }
